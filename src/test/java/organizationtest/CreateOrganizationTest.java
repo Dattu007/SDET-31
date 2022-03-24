a6c26583513a921408854ccbae354aca0a2382ba
dell <dell@DESKTOP-CTRJ7QU>
2022-03-24 12:56:53 +0530

    User -1 first commit

---
+++ src/test/java/organizationtest/CreateOrganizationTest.java
@@ -0,0 +1,13 @@
+package organizationtest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganizationTest {
+
+	@Test
+	public void createOrg() {
+		
+		System.out.println("User 1 is created Organization");
+	}
+	
+}
