a522eeb108f4f027afa1b130002d9dfc98a48098
dell <dell@DESKTOP-CTRJ7QU>
2022-03-28 12:26:33 +0530

    Latest Updated Two class Created

---
+++ src/test/java/organizationtest/CreateOrgWithDateTest.java
@@ -0,0 +1,12 @@
+package organizationtest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgWithDateTest {
+	
+	@Test
+	public void createOrgWithDate() {
+		System.out.println("created Org With Date");
+	}
+
+}
---
+++ src/test/java/organizationtest/CreateOrgWithIndustryTest.java
@@ -0,0 +1,13 @@
+package organizationtest;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgWithIndustryTest {
+	
+	@Test
+	public void createOrgWithInd() {
+		
+		System.out.println("Org created with Industry");
+	}
+
+}
